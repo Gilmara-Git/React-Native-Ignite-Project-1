bf0f21d6a891e25834fedaa03c1442d2ba199a93
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-02 17:58:16 -0500

    Testing StatusBar

--- eventManagement/App.tsx
+++ eventManagement/App.tsx
@@ -1,28 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
-import { useFonts,Inter_400Regular, Inter_700Bold,}from '@expo-google-fonts/inter';
-
+import { StatusBar } from "expo-status-bar";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
-  const [ fontsLoaded ]  = useFonts({
-    Inter_400Regular, Inter_700Bold
-})
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
 
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
 
-if(!fontsLoaded){
-  return <Loading/>
-}
-
-
-  return (      
-      <> 
-      <StatusBar 
-        style='light'
-        backgroundColor='transparent'
-        translucent
-      />
-      <Home/>
-      </>
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Home />
+    </>
   );
 }
