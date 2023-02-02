ae171802048af8722376c70aae9c5cec90202988
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-02 11:40:34 -0500

    Refactor: created a Defautl button and re-used it

---
+++ eventManagement/src/components/DefaultButton/styles.ts
@@ -0,0 +1,18 @@
+import themes from '../../themes/themes';
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    
+    button:{
+        height: 58,
+        width: 58,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily:themes.fonts.InterBold700
+    }
+
+})
+
+export default styles;
--- eventManagement/src/screens/Home/index.tsx
+++ eventManagement/src/screens/Home/index.tsx
@@ -1,92 +1,147 @@
-import styles from './styles';
-import themes from '../../themes/themes';
-import { View, Text , TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import { Participant } from '../../components/Participant';
-import { useState } from 'react';
+import styles from "./styles";
+import themes from "../../themes/themes";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Participant } from "../../components/Participant";
+import { useState } from "react";
+import { DefaultButton } from '../../components/DefaultButton';
+import {
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  Alert,
+  NativeSyntheticEvent,
+} from "react-native";
 
-export const Home =()=>{
-    const [ participants, setParticipants ] = useState<string[]>([]);
-    const [ newParticipant, SetNewParticipant ]  = useState('');
-  
-    
-    
-    const addParticipantHandler =()=> {
-    if(participants.includes(newParticipant)){
-       return  Alert.alert('Participant found.', `${newParticipant} is already enrolled on this Event`);
+export const Home = () => {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [newParticipant, SetNewParticipant] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [ inputFocusedBorder, setInputFocusedBorderColor ] = useState('');
+
+  const inputOnFocus =()=>{
+    setInputFocusedBorderColor(themes.colors.purple)
+  }
+  const inputOnBlur =()=>{
+    setInputFocusedBorderColor('')
+  }
+
+  const addParticipantHandler = () => {
+    if (participants.includes(newParticipant)) {
+      SetNewParticipant("");
+
+      return Alert.alert(
+        "Participant found.",
+        `${newParticipant} is already enrolled on this Event`
+      );
     }
 
-    if(!newParticipant.trim()){
-        return Alert.alert("Please enter a participant's name")
+    if (!newParticipant.trim()) {
+      return Alert.alert("Please enter a participant's name");
     }
-    setParticipants((prevState)=>[...prevState, newParticipant])
-    SetNewParticipant('')
+    setParticipants((prevState) => [...prevState, newParticipant]);
+    SetNewParticipant("");
+  };
 
-}
-   
+  const removeParticipantHandler = (participant: string) => {
+    Alert.alert(
+      "Delete?",
+      `Do you want to delete ${participant.toUpperCase()} from this Event ?`,
+      [
+        {
+          text: "Yes",
+          onPress: () =>
+            setParticipants((prevState) =>
+              prevState.filter(
+                (currentParticipant) => currentParticipant !== participant
+              )
+            ),
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ]
+    );
+  };
 
-    const removeParticipantHandler =(participant : string)=>{
-       Alert.alert('Delete?',  `Do you want to delete ${participant} from this Event ?`, [
-            {
-            text: 'Yes',
-            onPress: ()=> setParticipants((prevState)=> prevState.filter(currentParticipant => currentParticipant !== participant))   
-             },
-             { 
-                text: 'Cancel',
-                style: 'cancel'
-            }
-    ])
+  const updateEvent = (event: NativeSyntheticEvent) => {
+    setEventName(event.nativeEvent.text);
+  };
 
-        
-        
-    }
-    return (
+  return (
     <View style={styles.homeContainer}>
-        <Text 
-            style={styles.eventLabel}>
-            React Native Conference
-            </Text>
-        <Text 
-            style={styles.eventDate}>
-            Saturday, Feb 4th 2023
-        </Text>
-        <View style={styles.form}>
+      {!eventName ? (
+        <View style={styles.eventContainer}>
+          <Text style={styles.eventLabel}>What is your Event's name</Text>
+          <TextInput
+            onEndEditing={updateEvent}
+            style={styles.eventInput}
+            placeholder="Tap Event's name, then Enter ->"
+            placeholderTextColor={themes.colors.gray}
+          />
+
+         
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.eventLabel}>{eventName.toUpperCase()}</Text>
+          <Text style={styles.eventDate}>Saturday, Feb 4th 2023</Text>
+          <View style={styles.form}>
             <TextInput
-                style={styles.input}
-                placeholder="Type a participant's name"
-                placeholderTextColor={themes.colors.gray}
-                onChangeText={SetNewParticipant}
-                value={newParticipant}
-                
-            
-                
-                />
-            <TouchableOpacity
-                onPress={addParticipantHandler}
-                activeOpacity={0.7}
-                style={styles.button}
-                >
-                <AntDesign name="plus" size={24} color={themes.colors.lightWhite}/>
-            </TouchableOpacity>
+              style={[styles.input, { borderWidth:1, borderColor: inputFocusedBorder}]}
+              placeholder="Type a participant's name"
+              placeholderTextColor={themes.colors.gray}
+              onChangeText={SetNewParticipant}
+              value={newParticipant}
+              onFocus={inputOnFocus}
+              onBlur={inputOnBlur}
+            />
+              <DefaultButton
+                    color={themes.colors.greenButton} 
+                    participantHandler={addParticipantHandler}
+                    >
+                     
+                  <AntDesign name="plus" size={24} color={themes.colors.lightWhite} />
+              </DefaultButton>
+          </View>
 
-            </View>
-
-            <FlatList
+          <FlatList
+          contentContainerStyle={{flexGrow:1}}
             data={participants}
-            keyExtractor={item => item}
-            renderItem={({item})=> (
-                <Participant 
-                name={item} 
-                onRemove={()=>removeParticipantHandler(item)}
-                />)}
-                showsVerticalScrollIndicator={false}
-                ListEmptyComponent={()=>(
-              
-                    <Text style={styles.emptyList}>
-                        Add participants. You still don't have anybody on your Event List. 
+            keyExtractor={(item) => item}
+            renderItem={({ item }) => (
+              <Participant
+                name={item}
+                onRemove={removeParticipantHandler}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={() => (
+              <View>
+                <View style={styles.marginLine}></View>
+                <View style={styles.fallbackContainer}>
+                  <Text style={styles.emptyListText}>Empty Event list.</Text>
+                  <Text style={styles.emptyListText}>
+                    Add participants to your{" "}
+                    <Text style={styles.eventText}>
+                      {eventName.toUpperCase()}
                     </Text>
-          )}
-             />
-             
-    </View>)
+                  </Text>
+                  <View style={styles.listIcon}>
+                    <Ionicons
+                      name="list-circle-outline"
+                      size={80}
+                      color={themes.colors.gray}
+                    />
+                  </View>
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
